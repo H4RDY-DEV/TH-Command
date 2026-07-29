8a49df5ea7e6be5afa0a12277a27587882a871a6
Tyler Hardy <80357856+H4RDY-DEV@users.noreply.github.com>
2026-07-29 12:37:57 +0100

    Add files via upload

---
+++ PATCH_NOTES.md
@@ -0,0 +1,3 @@
+v9.0.3 Patch
+
+This patch is a scaffold. Ready for integration.
--- assets/js/services/jobs.js
+++ assets/js/services/jobs.js
@@ -1,22 +1,2 @@
-import {Storage} from "../core/storage.js";
-import {Events} from "../core/events.js";
-import {Job} from "../models/Job.js";
-import {HistoryService} from "./history.js";
-const KEY="th_command_jobs_v9";
-export const JobService={
- all(){return Storage.load(KEY,[]).map(j=>new Job(j));},
- save(job){
-  const jobs=this.all();
-  const i=jobs.findIndex(x=>x.id===job.id);
-  if(i>=0) jobs[i]=new Job(job); else jobs.push(new Job(job));
-  Storage.save(KEY,jobs);
-  HistoryService.log("Job Saved",{jobId:job.id,name:job.name});
-  Events.emit("jobs:changed",{count:jobs.length});
- },
- remove(id){
-  const jobs=this.all().filter(j=>j.id!==id);
-  Storage.save(KEY,jobs);
-  HistoryService.log("Job Removed",{jobId:id});
-  Events.emit("jobs:changed",{count:jobs.length});
- }
-};
+// v9.0.3 placeholder JobService integrated patch
+export * from './jobs.js';
